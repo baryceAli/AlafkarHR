add52e785835ebd7ff5ade0727450b3a42531e14
bashir mohamed ali <baryce_1@hotmail.com>
2026-04-22 12:34:01 +0300

    prepared Employee View

---
+++ ALAFKARHR/src/Modules/Auth/Auth/Users/Features/IsAlive/IsAliveEndpoint.cs
@@ -0,0 +1,17 @@
+﻿namespace Auth.Users.Features.IsAlive;
+
+public class IsAliveEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/", async () =>
+        {
+            return Results.Ok("Alafkar API is Alive");
+        })
+            .WithName("IsAlive")
+            .Produces(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("IsAlive")
+            .WithDescription("IsAlive");
+    }
+}
