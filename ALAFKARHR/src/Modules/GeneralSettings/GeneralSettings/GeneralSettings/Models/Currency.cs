ce89c54fe51c3754545dbb0f17350c80e01f5e6b
bashir mohamed ali <baryce_1@hotmail.com>
2026-06-10 16:56:38 +0300

    Set the default currency

--- ALAFKARHR/src/Modules/GeneralSettings/GeneralSettings/GeneralSettings/Models/Currency.cs
+++ ALAFKARHR/src/Modules/GeneralSettings/GeneralSettings/GeneralSettings/Models/Currency.cs
@@ -11,7 +11,7 @@ public class Currency:Aggregate<Guid>
     public string Symbol { get; set; }
     public bool IsDefault { get; set; }
     public Guid CompanyId { get; set; }
-    public static Currency Create(Guid id,string code, string name,string nameEng, decimal value,string symbol, string user)
+    public static Currency Create(Guid id,string code, string name,string nameEng, decimal value,string symbol, bool isDefault,Guid companyId, string user)
     {
         return new Currency
         {
@@ -21,6 +21,8 @@ public class Currency:Aggregate<Guid>
             NameEng = nameEng,
             Value = value,
             Symbol = symbol,
+            IsDefault = isDefault,
+            CompanyId = companyId,
             CreatedAt = DateTime.UtcNow,
             CreatedBy = user
         };
